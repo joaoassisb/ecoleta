c1a7a81fd9aa40a8c580d4c8ed47caa50250c2bb
João Vitor <joao@geolabor.com.br>
2020-06-06 12:44:54 -0300

    feat: finish mobile app

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,7 @@
 import express, { request, response } from "express";
+import multer from "multer";
 
+import multerConfig from "../config/multer";
 import PointsController from "./controllers/pointsController";
 import ItemsController from "./controllers/itemsController";
 
@@ -7,9 +9,11 @@ const router = express.Router();
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
+const upload = multer(multerConfig);
+
 router.get("/items", itemsController.list);
 
-router.post("/points", pointsController.create);
+router.post("/points", upload.single("image"), pointsController.create);
 router.get("/points", pointsController.list);
 router.get("/points/:id", pointsController.show);
 
